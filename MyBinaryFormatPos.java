ef0095d51d11b8f4048a20d8b10d68c618c246bb
alessiomodonesi <alessiomodo2004@gmail.com>
2023-10-20 11:55:54 +0200

    sistemati es1

--- MyBinaryFormatPos.java
+++ MyBinaryFormatPos.java
@@ -1,4 +1,3 @@
-package es1a;
 
 import java.util.Scanner;
 
@@ -6,19 +5,18 @@ public class MyBinaryFormatPos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Inserisci un numero da 0 a  127");
+        System.out.println("Inserisci un numero da 0 a 127");
         int numero = sc.nextInt();
         sc.close();
         System.out.println(byteToInt(numero, ""));
-
     }
 
     public static String byteToInt(int num, String num_b) {
-        if (num_b.length() >= 9) {
+        if (num_b.length() >= 9)
             return "0b" + num_b;
-        } else if (num_b.length() == 4) {
+        else if (num_b.length() == 4)
             num_b = "_" + num_b;
-        }
+
         num_b = (num % 2) + num_b;
         num = num / 2;
         return byteToInt(num, num_b);
