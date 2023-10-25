13192c4b9e03f2c1be0fe9d2ea6ef22e5c619bf7
alessiomodonesi <alessiomodo2004@gmail.com>
2023-10-25 17:12:58 +0200

    test javadoc

--- BankAccount/BankAccount.java
+++ BankAccount/BankAccount.java
@@ -1,4 +1,8 @@
 public class BankAccount {
+    /**
+     * Un conto bancario ha un saldo
+     * modificabile tramite depositi e prelievi
+     */
     public BankAccount() {
         balance = 0;
     }
@@ -11,10 +15,20 @@ public class BankAccount {
         balance = balance + amount;
     }
 
+    /**
+     * Preleva denaro dal conto
+     * 
+     * @param amount importo da prelevare
+     */
     public void withdraw(double amount) {
         balance = balance - amount;
     }
 
+    /**
+     * Ispeziona saldo attuale
+     * 
+     * @return saldo attuale
+     */
     public double getBalance() {
         return balance;
     }
