6842497255d98aca176c83e28409092d33f52db6
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-03 11:43:07 +0100

    Create PrimeNumberRecognizer.java

---
+++ Esecitazione2/PrimeNumberRecognizer.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrimeNumberRecognizer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0, p = 0;
+
+        System.out.print("inserisci n: ");
+        n = sc.nextInt();
+        sc.close();
+
+        for (int i = 2; i <= n / 2; i++) {
+            if ((n % i) == 0) {
+                System.out.println(String.format("il numero %d é divisibile per %d", n, i));
+                System.exit(0);
+            }
+        }
+
+        System.out.println(String.format("il numero %d é primo", n));
+    }
+}
