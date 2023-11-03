b05e8297737b373bcd24890c9adcad11e9785ea9
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-03 11:04:58 +0100

    Create MultipleLister.java

---
+++ Esecitazione2/MultipleLister.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MultipleLister {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0, max = 0, res = 0, i = 1;
+
+        do {
+            System.out.print("inserisci n > 0: ");
+            n = sc.nextInt();
+        } while (n <= 0);
+
+        do {
+            System.out.print("inserisci max > n: ");
+            max = sc.nextInt();
+        } while (max <= n);
+
+        sc.close();
+        System.out.println(String.format("multipli di %d in [%d, %d]", n, n, max));
+
+        do {
+            res = n * i++;
+            if (res <= max)
+                System.out.println(res);
+        } while (res <= max);
+    }
+}
