d1eae18c8f1d441e5188ba5d85148db21e95cc69
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-06 09:17:06 +0100

    finito es factor resolver

--- Esecitazione2/FactorResolver.java
+++ Esecitazione2/FactorResolver.java
@@ -2,14 +2,29 @@ import java.util.Scanner;
 
 public class FactorResolver {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = 0;
+        Scanner in = new Scanner(System.in);
+        String s = "1";
+        int n = 0, j = 2;
 
         do {
-            System.out.print("inserisci n: ");
-            n = sc.nextInt();
+            System.out.print("n?: ");
+            n = in.nextInt();
         } while (!(n > 1));
-        sc.close();
+        in.close();
 
+        int k = n;
+
+        while (j * j <= k) {
+            while (k % j == 0) {
+                s = s + " * " + j;
+                k = k / j;
+            }
+            j++;
+        }
+
+        if (k > 1)
+            s += " * " + k;
+
+        System.out.println(n + " = " + s);
     }
 }
