9957333416f6f45df2d7ac30f3a03f7cc1ef9ae3
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-08 10:56:46 +0100

    caricati es lab2

--- EsLab/Lab1/Es8.java
+++ EsLab/Lab1/Es8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Es1Lab {
+public class Es8 {
     public static void main(String[] args) {
         String tmp = "";
         Scanner input = new Scanner(System.in);
