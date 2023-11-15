d89db72bb2311c9953381af44ba55783604d99bc
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-15 10:53:20 +0100

    sistemata dir

--- EsLab/Lab2/Es7.java
+++ EsLab/Lab2/Es7.java
@@ -1,18 +1,9 @@
-import java.util.Scanner;
-
 public class Es7 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Inserisci il primo orario (formato 24 ore, es. 0900 o 1730): ");
-        int primoOrario = input.nextInt();
-
-        System.out.print("Inserisci il secondo orario (formato 24 ore, es. 0900 o 1730): ");
-        int secondoOrario = input.nextInt();
-
-        input.close();
-
+        int primoOrario = 1000;
+        int secondoOrario = 1200;
         int tempoTrascorso = 0;
+
         if (secondoOrario >= primoOrario)
             tempoTrascorso = secondoOrario - primoOrario;
         else
---
+++ EsLab/Lab3/Es1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Es1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Inserisci il primo orario (formato 24 ore, es. 0900 o 1730): ");
+        int primoOrario = input.nextInt();
+
+        System.out.print("Inserisci il secondo orario (formato 24 ore, es. 0900 o 1730): ");
+        int secondoOrario = input.nextInt();
+
+        input.close();
+
+        int tempoTrascorso = 0;
+        if (secondoOrario >= primoOrario)
+            tempoTrascorso = secondoOrario - primoOrario;
+        else
+            tempoTrascorso = (2400 - primoOrario) + secondoOrario;
+
+        int ore = tempoTrascorso / 100;
+        int minuti = tempoTrascorso % 100;
+
+        System.out.println("Tempo trascorso: " + ore + " ore e " + minuti + " minuti");
+    }
+}
