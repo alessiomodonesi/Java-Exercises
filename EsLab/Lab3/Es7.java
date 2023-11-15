3ca1a41f2d3bcda5e6d74bd0c43d06077cf1c0cd
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-15 11:52:44 +0100

    Update Es7.java

--- EsLab/Lab3/Es7.java
+++ EsLab/Lab3/Es7.java
@@ -1,3 +1,21 @@
-public class Es7 {
+import java.util.Scanner;
 
+public class Es7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("a?: ");
+        double a = input.nextDouble();
+        System.out.print("b?: ");
+        double b = input.nextDouble();
+        input.close();
+
+        double c = Math.hypot(a, b);
+        double perimeter = a + b + c;
+        double area = (a * b) / 2;
+        double alpha = Math.toDegrees(Math.asin(a / c));
+        double beta = Math.toDegrees(Math.acos(a / c));
+
+        System.out.println("Ipotenusa: %.2f cm \nPerimetro: %.2f cm \nArea: %.2f cm2 \nAlpha: %.2f° \nBeta: %.2f°"
+                .formatted(c, perimeter, area, alpha, beta));
+    }
 }
