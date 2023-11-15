7aaca97e4a63b6fa776f98e6292dc609e1f0056c
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-15 12:14:02 +0100

    Update Es8.java

--- EsLab/Lab3/Es8.java
+++ EsLab/Lab3/Es8.java
@@ -4,7 +4,7 @@ public class Es8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("a?: ");
-        double alpha = input.nextDouble();
+        double alpha = Math.toRadians(input.nextDouble());
         input.close();
 
         // Normalize the angle in the range (-2π, 2π)
@@ -15,9 +15,9 @@ public class Es8 {
                 alpha -= 2 * Math.PI;
         }
 
-        System.out.println("angolo = %.3f rad\n".formatted(alpha / Math.PI) + //
-                "sin(0.250*π) = %.3f\n".formatted(Math.sin(alpha)) + //
-                "cos(0.250*π) = %.3f\n".formatted(Math.cos(alpha)) + //
-                "tan(0.250*π) = %.3f".formatted(Math.tan(alpha)));
+        System.out.println("angolo = %.3f rad\n".formatted(alpha) + //
+                "sin(alpha) = %.3f\n".formatted(Math.sin(alpha)) + //
+                "cos(alpha) = %.3f\n".formatted(Math.cos(alpha)) + //
+                "tan(alpha) = %.3f".formatted(Math.tan(alpha)));
     }
 }
