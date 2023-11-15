9449b1b992f1a8eb4c50dd120d202139eb4c97bd
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-15 10:50:56 +0100

    Update Es7.java

--- EsLab/Lab3/Es7.java
+++ EsLab/Lab3/Es7.java
@@ -2,39 +2,25 @@ import java.util.Scanner;
 
 public class Es7 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = 0, n_hour = 0, n_min = 0;
-        int m = 0, m_hour = 0, m_min = 0;
-        int tot_hour = 0, tot_min = 0;
-        String time_start = "", time_end = "";
+        Scanner input = new Scanner(System.in);
 
-        do {
-            System.out.print("start: ");
-            n = in.nextInt();
-        } while ((n < 0000 || n > 2359) || n % 100 >= 60);
+        System.out.print("Inserisci il primo orario (formato 24 ore, es. 0900 o 1730): ");
+        int primoOrario = input.nextInt();
 
-        do {
-            System.out.print("end: ");
-            m = in.nextInt();
-        } while ((m < 0000 || m > 2359) || m % 100 >= 60);
-        in.close();
+        System.out.print("Inserisci il secondo orario (formato 24 ore, es. 0900 o 1730): ");
+        int secondoOrario = input.nextInt();
 
-        n_hour = n / 100;
-        m_hour = m / 100;
-        n_min = n % 100;
-        m_min = m % 100;
+        input.close();
 
-        if (n >= m) {
-            tot_hour = m_hour - n_hour;
-            tot_min = m_min - n_min;
-        } else {
-            tot_hour = (24 - n_hour) + m_hour;
-            tot_min = m_min - n_min;
-        }
+        int tempoTrascorso = 0;
+        if (secondoOrario >= primoOrario)
+            tempoTrascorso = secondoOrario - primoOrario;
+        else
+            tempoTrascorso = (2400 - primoOrario) + secondoOrario;
 
-        time_start = String.format("%d:%d", n_hour, n_min);
-        time_end = String.format("%d:%d", m_hour, m_min);
-        System.out.println(String.format("start: %s, stop: %s, time: %dh %dmin",
-                time_start, time_end, tot_hour, tot_min));
+        int ore = tempoTrascorso / 100;
+        int minuti = tempoTrascorso % 100;
+
+        System.out.println("Tempo trascorso: " + ore + " ore e " + minuti + " minuti");
     }
 }
