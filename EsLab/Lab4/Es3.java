3dacd388cb7fc2fb7ef39aece6f4eab8ec2576f0
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-22 10:59:44 +0100

    finito es3

--- EsLab/Lab4/Es3.java
+++ EsLab/Lab4/Es3.java
@@ -1,3 +1,15 @@
-public class Es3 {
+import java.util.Scanner;
 
+public class Es3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("year?: ");
+        int year = input.nextInt();
+        input.close();
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            System.out.println(year + " è un anno bisestile");
+        else
+            System.out.println(year + " non è un anno bisestile");
+    }
 }
