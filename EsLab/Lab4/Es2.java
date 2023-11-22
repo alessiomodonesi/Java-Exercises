a156b328c439af9df7a571037e3f4e49756e53ae
alessiomodonesi <alessiomodo2004@gmail.com>
2023-11-22 10:57:48 +0100

    finito es2

--- EsLab/Lab4/Es2.java
+++ EsLab/Lab4/Es2.java
@@ -1,3 +1,19 @@
-public class Es2 {
+import java.util.Scanner;
 
+public class Es2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n1?: ");
+        double n1 = input.nextDouble();
+        System.out.print("n2?: ");
+        double n2 = input.nextDouble();
+        input.close();
+
+        if (Math.abs(n1 - n2) < 0.0001) {
+            System.out.println("I due numeri sono approssimativamente uguali");
+        } else {
+            System.out.println("I due numeri non sono approssimativamente uguali");
+        }
+    }
 }
