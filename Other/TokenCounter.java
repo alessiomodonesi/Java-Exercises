12256597412f5bf4fea58d0cd6c0afa0f9df921e
alessiomodonesi <alessiomodo2004@gmail.com>
2023-12-08 16:59:28 +0100

    Update TokenCounter.java

--- Other/TokenCounter.java
+++ Other/TokenCounter.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class TokenCounter {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         int count = 0;
-        while (in.hasNextLine()) {
-            String line = in.nextLine();
-            Scanner t = new Scanner(line);
-            while (t.hasNext()) {
-                t.next(); // non devo elaborare
-                count++;
-            }
-            t.close();
-        }
+        Scanner in = new Scanner(System.in);
+        Scanner t = new Scanner(in.nextLine());
         in.close();
+
+        while (t.hasNext()) {
+            t.next(); // non devo elaborare
+            count++;
+        }
+
+        t.close();
         System.out.println(count + " token");
     }
 }
