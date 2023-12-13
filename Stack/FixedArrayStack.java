fa5623d9f8ad4f51142a366b34b2e24e416e4680
alessiomodonesi <alessiomodo2004@gmail.com>
2023-12-13 17:24:27 +0100

    sistemata dir stack

---
+++ Stack/FixedArrayStack.java
@@ -0,0 +1,36 @@
+public class FixedArrayStack implements Stack {
+    public FixedArrayStack() {
+        v = new Object[INITSIZE];
+        makeEmpty();
+    }
+
+    public void makeEmpty() {
+        vSize = 0;
+    }
+
+    public boolean isEmpty() {
+        return (vSize == 0);
+    }
+
+    public void push(Object obj) {
+        if (vSize == v.length)
+            throw new FullStackException();
+        v[vSize++] = obj;
+    }
+
+    public Object top() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        return v[vSize - 1];
+    }
+
+    public Object pop() {
+        Object obj = top();
+        vSize--;
+        return obj;
+    }
+
+    protected Object[] v;
+    protected int vSize;
+    public static final int INITSIZE = 100;
+}
