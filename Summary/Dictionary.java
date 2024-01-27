5e5c9bbd515706b8deb20060894c5575a677ec78
alessiomodonesi <alessiomodo2004@gmail.com>
2024-01-27 12:05:31 +0100

    Update Dictionary.java

--- Summary/Dictionary.java
+++ Summary/Dictionary.java
@@ -1,31 +1,31 @@
-/**
- * Chiavi ordinate
- * insert(): O(n), O(n log n) con altro algoritmo
- * remove(): O(n)
- * find(): O(log n)
- * 
- * Chiavi NON ordinate
- * insert(): O(n)
- * remove(): O(n), O(1) per chiavi non uniche
- * find(): O(n)
- */
-
 @SuppressWarnings("rawtypes")
 public interface Dictionary extends Container {
-    // l'inserimento va sempre a buon fine; se la chiave non
-    // esiste la coppia viene aggiunta al dizionario. Se esiste,
-    // il valore a essa associato viene sovrascritto dal nuovo
-    // valore; se key è null si lancia IllegalArgumentException
+    /**
+     * se la chiave NON esiste la coppia viene aggiunta al dizionario
+     * se esiste, il valore a essa associato viene sovrascritto dal nuovo valore
+     * 
+     * T(n) = O(n), O(n log n) con altro algoritmo
+     * 
+     * @exception IllegalArgumentException se key è null
+     */
     void insert(Comparable key, Object value);
 
-    // la rimozione della chiave rimuove anche il corrispondente
-    // valore dal dizionario. Se la chiave non esiste si lancia
-    // DictionaryItemNotFoundException
+    /**
+     * la rimozione della chiave rimuove anche il
+     * corrispondente valore dal dizionario
+     * 
+     * T(n) = O(n), O(1) per chiavi non uniche
+     * 
+     * @exception DictionaryItemNotFoundException se la chiave non esiste
+     */
     void remove(Comparable key);
 
-    // la ricerca per chiave restituisce (solo) il valore a essa
-    // associato nel dizionario. Se la chiave non esiste si
-    // lancia DictionaryItemNotFoundException
+    /**
+     * T(n) = O(n), O(log n) per chiavi ordinate
+     * 
+     * @exception DictionaryItemNotFoundException se la chiave non esiste
+     * @return valore associato alla chiave
+     */
     Object find(Comparable key);
 }
 
