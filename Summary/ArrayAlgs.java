182361ddbaa3a58e3a54a61e9939a309961cfbb6
alessiomodonesi <alessiomodo2004@gmail.com>
2024-01-29 20:28:28 +0100

    Update ArrayAlgs.java

--- Summary/ArrayAlgs.java
+++ Summary/ArrayAlgs.java
@@ -1,60 +1,5 @@
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class ArrayAlgs {
-    public static int[] insert(int[] v, int vSize, int index, int value) {
-        if (vSize == v.length)
-            v = resize(v, 2 * v.length);
-        for (int i = vSize; i > index; i--)
-            v[i] = v[i - 1];
-        v[index] = value;
-        return v;
-    }
-
-    public static int findMin(int[] v, int vSize) {
-        int min = v[0];
-        for (int i = 1; i < vSize; i++)
-            if (v[i] < min)
-                min = v[i];
-        return min;
-    }
-
-    public static int findMax(int[] v, int vSize) {
-        int max = v[0];
-        for (int i = 1; i < vSize; i++)
-            if (v[i] > max)
-                max = v[i];
-        return max;
-    }
-
-    /**
-     * Costruisce un array contenente valori casuali compresi tra 0 e n-1
-     * 
-     * @param lenght lunghezza dell'array
-     * @param n      valore massimo -1
-     * @return array di valori casuali
-     */
-    public static int[] randomIntArray(int length, int n) {
-        int[] v = new int[length];
-        for (int i = 0; i < v.length; i++)
-            // ricordiamoci come funziona Math.random()...
-            v[i] = (int) (n * Math.random());
-        return v;
-    }
-
-    /**
-     * Stampa tutti gli elementi di un array
-     * 
-     * @param v     array da stampare
-     * @param vSize dimensione dell'array v
-     * @return stringa contenente i valori dell'array
-     */
-    public static String printArray(int[] v, int vSize) {
-        String s = "[";
-        for (int i = 0; i < vSize; i++)
-            s = s + v[i] + " ";
-        s = s + "\b]";
-        return s;
-    }
-
     /**
      * Ridimensiona l'array oldv attribuendogli la lunghezza newLength
      * 
@@ -72,15 +17,6 @@ public class ArrayAlgs {
         for (int i = 0; i < count; i++)
             newv[i] = oldv[i];
         return newv;
-    }
-
-    public static void remove(int[] v, int vSize, int index) {
-        v[index] = v[vSize - 1];
-    }
-
-    public static void removeSorted(int[] v, int vSize, int index) {
-        for (int i = index; i < vSize - 1; i++)
-            v[i] = v[i + 1];
     }
 
     /**
@@ -209,41 +145,5 @@ public class ArrayAlgs {
             return binSearch(v, mid + 1, to, value);
         else // cerca a sinistra
             return binSearch(v, from, mid - 1, value);
-    }
-
-    /**
-     * Genera un array bidimensionale con i
-     * valori delle potenze di x^y.
-     * 
-     * @param x numero di righe
-     * @param y numero di colonne
-     * @return matrice
-     */
-    public static int[][] generatePowers(int x, int y) {
-        int[][] powers = new int[x][y];
-        for (int i = 0; i < x; i++)
-            for (int j = 0; j < y; j++)
-                powers[i][j] = (int) Math.round(Math.pow(i + 1, j + 1));
-        return powers;
-    }
-
-    /**
-     * Visualizza un array bidimensionale di
-     * numeri interi con colonne di larghezza
-     * fissa e valori allineati a destra.
-     * 
-     * @param v     matrice
-     * @param width numero di colonne
-     */
-    public static void printPowers(int[][] v, int width) {
-        for (int i = 0; i < v.length; i++) {
-            for (int j = 0; j < v[i].length; j++) {
-                String s = Integer.toString(v[i][j]);
-                while (s.length() < width)
-                    s = " " + s;
-                System.out.print(s);
-            }
-            System.out.println();
-        }
     }
 }
